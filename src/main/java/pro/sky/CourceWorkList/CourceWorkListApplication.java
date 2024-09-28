23e7ee28d67ef89132e9b32f4c23a027b2990caf
JTK1701 <jonmalkon@mail.ru>
2024-09-29 02:50:35 +0300

    start

---
+++ src/main/java/pro/sky/CourceWorkList/CourceWorkListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CourceWorkList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourceWorkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourceWorkListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/CourceWorkList/CourceWorkListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CourceWorkList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourceWorkListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
